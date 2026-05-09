ccfa214b6853304fc453eb01ef31fcbf3161f25c
2017000096-MarcoPineda <m.pineda.5bdb@gmail.com>
2026-05-09 16:47:41 -0600

    Add files via upload

---
+++ Visual-G4-U2/Leccion11/act2_ins2_lecc11.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero = 1;
+
+        do
+        {
+            Console.WriteLine(numero);
+            numero = numero + 2;
+        }
+        while (numero <= 100);
+
+        Console.ReadKey();
+    }
+}
